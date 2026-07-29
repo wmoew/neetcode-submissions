a1ae9df17e87924c4602f893355cd5ea7cd8daf6
Moe <43256315+wmoew@users.noreply.github.com>
2026-07-29 18:48:23 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        const sortedS = s.split('').sort().join('');
+        const sortedT = t.split('').sort().join('');
+        if (sortedS == sortedT) return true;
+        return false;
+        
+}
+}
